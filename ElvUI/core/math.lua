dc354e6b5921582626ef2b7118c5575ad6434537
datbunneh <datbunneh@hotmail.com>
2017-01-09 20:16:27 +0200

    Return shortened millions

--- ElvUI/core/math.lua
+++ ElvUI/core/math.lua
@@ -21,8 +21,8 @@ function E:ShortValue(v)
 	if(E.db.general.numberPrefixStyle == "METRIC") then
 		if(abs(v) >= 1e9) then
 			return format("%.1fG", v / 1e9);
---		elseif(abs(v) >= 1e6) then
---			return format("%.1fM", v / 1e6);
+		elseif(abs(v) >= 1e6) then
+			return format("%.1fM", v / 1e6);
 --		elseif(abs(v) >= 1e3) then
 --			return format("%.1fk", v / 1e3);
 		else
